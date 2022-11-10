c9ff91ddfcdcb5c4d5570acd5efe55f7176156d1
Joel Stobart <joel.stobart@landregistry.gov.uk>
2022-11-10 21:27:41 +0000

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import {Helmet} from "react-helmet"
+import "@fontsource/audiowide"
+
+export default function Home() {
+  return <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Stob.art</title>
+      <link rel="canonical" href="https://stob.art" />
+      <link rel="stylesheet" type="text/css" href="style.css"/>
+    </Helmet>
+    <div className="container">
+      <h1>Stob.art</h1>
+    </div>
+  </div>
+}
