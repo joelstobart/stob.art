7cef6abec5248ab35918e3eefb24a0113b400b2a
Joel Stobart <joel.stobart@landregistry.gov.uk>
2022-11-10 21:52:22 +0000

    Tweak

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <title>Stob.art</title>
       <link rel="canonical" href="https://stob.art" />
       <link rel="stylesheet" type="text/css" href="style.css"/>
-      <link rel="me" href="https://mastodon.green/@joel">Mastodon</link>
+      <link rel="me" href="https://mastodon.green/@joel"/>
     </Helmet>
     <div className="container">
       <h1>Stob.art</h1>
